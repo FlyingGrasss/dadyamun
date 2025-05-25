b185a30de73850dcce8cf4e42d9ebaa9786a01ae
FlyingGrass <emre.bozqurt@gmail.com>
2025-05-25 23:16:22 +0300

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "@/app/globals.css";
-
+import { Analytics } from "@vercel/analytics/next";
 export const metadata: Metadata = {
   title: {
     default: "DADYAMUN'25",
