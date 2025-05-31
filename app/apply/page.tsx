2df791f1822d107eadd3e9b81ae411f6783c618e
FlyingGrass <emre.bozqurt@gmail.com>
2025-05-31 14:16:43 +0300

    apply closed for the time being

--- app/apply/page.tsx
+++ app/apply/page.tsx
@@ -29,6 +29,24 @@ const Apply = () => {
   return (
     <>
 
+      <div className='flex flex-col h-[80vh] justify-center items-center gap-10 max-sm:gap-6'>
+          <h1 className="text-7xl max-sm:text-5xl text-center text-[#172D7F] font-bold">Coming Soon</h1>
+          <Link href="/" className="w-fit">
+            <button className="group max-sm:text-base text-xl hover:text-[#172D7F] cursor-pointer items-center transition-colors justify-center gap-4 max-sm:gap-2 inline-flex bg-[#0c0c0c] text-[#ffffff] rounded-full px-8 max-sm:px-6 max-sm:py-3 py-4">
+              Home
+              <Image
+                src="/arrowWhite.svg" 
+                width={20} 
+                height={16} 
+                alt=""
+                className="transition-transform duration-400 max-sm:w-[15px] h-auto group-hover:translate-x-2"
+              />
+              
+            </button>
+          </Link>
+        </div>
+    </>
+    /* 
       <h1 className="text-6xl max-sm:text-3xl mt-16 mb-16 max-sm:mt-8 text-center text-[#172D7F] font-bold">Apply</h1>
 
       <div className="grid place-items-center w-full grid-cols-2 sm:grid-cols-3 gap-6 sm:gap-16 px-4 sm:px-0 mt-12 mb-12 sm:mb-16 sm:mt-16">
@@ -135,8 +153,10 @@ const Apply = () => {
           
       </div>
 
+    */
+
       
-    </>
+    
   );
 };
 
