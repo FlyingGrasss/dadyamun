0e9a363fc46f80604278279e07e4297e6aa640e3
Emre Bozkurt <157544287+FlyingGrasss@users.noreply.github.com>
2025-06-06 23:38:34 +0300

    Update page.tsx

--- app/apply/delegation/page.tsx
+++ app/apply/delegation/page.tsx
@@ -370,6 +370,7 @@ const ApplyDelegation = () => {
                         required
                       >
                         <option value="">Select grade/level</option>
+                        <option value="Secondary (Middle) School">Secondary (Middle) School</option>
                         <option value="Preparation Grade">Preparation</option>
                         <option value="9th Grade">9th Grade</option>
                         <option value="10th Grade">10th Grade</option>
